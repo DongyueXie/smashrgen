a24586345c41950c51429360f495a97acf93c0f0
Dongyue Xie <dongyxie@gmail.com>
2020-02-02 20:02:57 -0600

    revise

--- R/init_smash_gen.R
+++ R/init_smash_gen.R
@@ -4,15 +4,17 @@
 #' @param ntri: Binomial number of trials
 #' @param ashp: whether apply ash firstly
 #' @param dist_family: family of ditribution: 'poisson','binomial','poi_binom'.
+#' @param epsilon: adjustment if x/ntri=1. adjusted to be 1-epsilon.
 #' @return initilized m,s,y
 #' @export
 
 
-init_smash_gen=function(x,ntri,ashp,dist_family){
+init_smash_gen=function(x,ntri,ashp,dist_family,epsilon=1e-4){
   n=length(x)
   if(dist_family=='poisson'|dist_family=='poi_binom'){
     if(ashp){
       m0=ash(rep(0,n),1,lik=lik_pois(x))$result$PosteriorMean
+      m0[which(x!=0)]=(x[which(x!=0)])
     }else{
       m0=rep(mean(x),n)
     }
@@ -28,6 +30,8 @@ init_smash_gen=function(x,ntri,ashp,dist_family){
   if(dist_family=='binomial'){
     if(ashp){
       m0=ash(rep(0,n),1,lik=lik_binom(x,ntri))$result$PosteriorMean
+      m0[which(x!=0)]=(x/ntri)[which(x!=0)]
+      m0[which(m0==1)]=1-epsilon
     }else{
       m0=rep(mean(x/ntri),n)
     }
